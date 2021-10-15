dafe10a1c7d1e0a8200c03da1447e55c15230c8f
Jyboer <onischuk.mihail@gmail.com>
2021-10-15 16:48:02 +0600

    feat: (15.10.2021) Added webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
-  
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].[hash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 8080
+  }
 }
