909bfd45c1af16d148eab2310d61d56c90641db6
Jyboer <onischuk.mihail@gmail.com>
2021-10-18 14:31:05 +0600

    feat: (18.10.20021) Finish webpack configure

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,28 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './index.ts',
+  entry: './index.tsx',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
       },
     ],
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      }
-    ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
